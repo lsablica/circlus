6ad3a5c902a6e3cf273569f44c081122a774fda6
lsablica <lukas.sablica@gmail.com>
2024-01-10 12:41:35 +0100

    starting m step

---
+++ src/pkbd.cpp
@@ -0,0 +1,59 @@
+#include <RcppArmadillo.h>
+using namespace Rcpp;
+using namespace std;
+
+// [[Rcpp::export]]
+double hybridnewton(double c1, double c2, double c3, double tol = 1e-6, int maxiter = 100) {
+  double x,a,b;
+  a = 0;
+  b = 1;
+  x = 0.5;
+  
+  double g, dg, oldx = 1;
+  int niter = 0;
+  
+  while(tol < std::abs(x-oldx) && niter < maxiter){
+    oldx = x; 
+    g = x*c1+x*c2/(1-x*x)-c3;
+    dg = c1+c2*(1+x*x)/pow(1-x*x,2);
+    x -= g/dg;
+    if(x < a || x > b){
+      if(g>0){
+        b = oldx;
+      }
+      else{
+        a = oldx;
+      }
+      x= (b+a)/2;
+    }
+    niter +=1;
+  }
+  return x;
+}
+
+void M_step(const arma::mat &data, const arma::mat &beta_matrix, 
+            arma::mat &mu_matrix, arma::vec &rho_vector,
+            int k, int n, int d, double tol, int maxiter){ 
+  arma::rowvec alpha = sum(beta_matrix)/n;
+  
+      
+  arma::mat crossmat = data*mu_matrix;
+  arma::mat rho_mat;
+  rho_mat.each_row() = rho_vector;
+  arma::mat wscale_mat =  1 + pow(rho_mat, 2) - 2*rho_mat%crossmat;
+  arma::mat scaled_weight_matrix = beta_matrix/wscale_mat;  
+  arma::mat mu = scaled_weight_matrix.t() * data; 
+  arma::rowvec mu_norms = arma::vecnorm(mu, 2, 0);
+  mu_matrix = mu_norms.each_row()/mu_norms;
+  arma::rowvec sums_scaled_weight_matrix = sum(scaled_weight_matrix, 0);
+  //standardize each
+  double c1, c2, c3;
+  for(int i = 0; i < k; i++){
+    c1 = d*sums_scaled_weight_matrix(i);
+    c2 = 2*n*alpha(i);
+    c3 = d*mu_norms(i);
+    rho_vector(i) = hybridnewton(c1,c2,c3,tol,maxiter); 
+  }
+  
+}
+
---
+++ test.R
@@ -0,0 +1,105 @@
+numData = 500
+alpha_current_h = 0.2
+numVar = 50
+mu_denom_h = 2
+sum_h_weightMat = 10
+
+root_func <- function(x) x*numVar*sum_h_weightMat + (2*numData*alpha_current_h)*x/(1 - x^2) - numVar*mu_denom_h 
+
+
+rho_current <- (uniroot(
+  f = root_func,
+  interval = c(0,1),
+  f.lower = root_func(0),
+  f.upper = root_func(1),
+  tol = 1e-9
+))$root
+
+
+c1 = numVar*sum_h_weightMat
+c2 = (2*numData*alpha_current_h)
+c3 = numVar*mu_denom_h
+
+library(Rcpp)
+library(microbenchmark)
+sourceCpp("src/pkbd.cpp")
+hybridnewton(c1, c2,c3)
+
+microbenchmark((uniroot(
+  f = root_func,
+  interval = c(0,1),
+  f.lower = root_func(0),
+  f.upper = root_func(1),
+  tol = 1e-9
+))$root, hybridnewton(c1, c2,c3), times = 1000)
+
+
+
+
+dat <- matrix(rnorm(50*5), nrow = 50, ncol = 5)
+dat <- dat/sqrt(rowSums(dat^2))
+numClust <- 3 
+
+numVar <- ncol(dat)
+numData <- nrow(dat)
+
+alpha_current <- rep(1/numClust, numClust)
+rho_current <- rep(0.5,numClust)
+mu_current <- dat[sample(1:numData, size=numClust, replace=FALSE) ,]
+
+
+mu_matrix = t(mu_current)
+rho_vector = rho_current
+
+currentIter <- 1
+membCurrent <- rep.int(0, times = numData)
+loglikCurrent <- -Inf
+
+# Begin EM iterations
+{
+  v_mat <- dat %*% t(mu_current)
+  alpha_mat_current <- matrix(alpha_current,nrow = numData,ncol = numClust,byrow = TRUE)
+  rho_mat_current <- matrix(rho_current,nrow = numData,ncol = numClust,byrow = TRUE)
+  log_probMat_denom <- log(1 + rho_mat_current^2 - 2*rho_mat_current*v_mat)
+  # eq (11) in ICMLD16 paper
+  log_probMat <- log(1 - (rho_mat_current^2)) - (numVar / 2) * log_probMat_denom
+  ######### E step done#############################################
+  ########## M step now#############################################
+  # Denominator of eq (18) in ICMLD16 paper
+  probSum <- matrix(exp(log_probMat) %*% alpha_current,nrow = numData,ncol = numClust)
+  # eq (18) in ICMLD16 paper
+  log_normProbMat_current <- log(alpha_mat_current) + log_probMat - log(probSum)
+  beta_matrix = exp(log_normProbMat_current)
+  # denominator of eq (20) in ICMLD16 paper
+  log_weightMat <- log_normProbMat_current - log_probMat_denom
+  
+  # eq (19) in ICMLD16 paper
+  alpha_current <- colSums(exp(log_normProbMat_current)) / numData
+  # numerator of fraction in eq (21) of ICMLD16 paper
+  mu_num_sum_MAT <- t(exp(log_weightMat)) %*% dat
+  norm_vec <- function(x) sqrt(sum(x^2))
+  mu_denom <- apply(mu_num_sum_MAT, 1, norm_vec)
+  # eq (21) of ICMLD16 paper without sign function
+  mu_current <- mu_num_sum_MAT / mu_denom
+  for(h in 1:numClust){
+    # update rho params
+    sum_h_weightMat <- sum(exp(log_weightMat[,h]))
+    alpha_current_h <- alpha_current[h]
+    mu_denom_h <- mu_denom[h]
+    root_func <- function(x) {
+      (-2*numData*x*alpha_current_h)/(1 - x^2) + numVar*mu_denom_h -numVar*x*sum_h_weightMat
+    }
+    rho_current[h] <- (uniroot(
+      f = root_func,
+      interval = c(0,1),
+      f.lower = root_func(0),
+      f.upper = root_func(1),
+      tol = 0.001
+    ))$root
+  }
+  # Update counter to NEXT iteration number.
+  currentIter <- currentIter + 1
+}
+
+
+
